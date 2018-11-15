72f258b713ed5eab38c02a90298fdc1da4246077
Gabriel Bénédict <gbndict@gmail.com>
2018-11-15 12:58:05 -0500

    digrams start

--- lib/Digrams.R
+++ lib/Digrams.R
@@ -1,1 +1,11 @@
 load("../output/dict.RData")
+
+dict <- as.matrix(dict)
+
+dict <- dict[nchar(dict) > 1] # no single character words
+dict <- unique(dict)
+
+for(i in 1:length(dict)){
+  comb <- nchar(dict[i]) * (nchar(dict[i])-1) / 2
+}
+
