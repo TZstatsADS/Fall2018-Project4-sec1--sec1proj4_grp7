8de85e4ab3c5fea1042818db6229a9381c5c7e35
Gabriel Bénédict <gbndict@gmail.com>
2018-11-21 14:17:07 -0500

    detect

--- lib/detect.R
+++ lib/detect.R
@@ -4,6 +4,7 @@ load("../output/digrams.RData")
 
 
 d2 <- dict2[nchar(dict2[,1]) < 16,]
+d2 <- d2[nchar(d2[,1]) > 1,] # no single character words
 
 
 for(i in 1:nrow(d2)){
@@ -12,9 +13,9 @@ for(i in 1:nrow(d2)){
   for(k in 1 : (n-1)){
     for(l in 2:n){
       if(k < l){
-        print(cat(i, " ", k, " ", l))
+        cat(i, " ", k, " ", l)
         if(a[n, counter, match(substr(d2[i,1], k, k), letters), 
-          match(substr(d2[i,1], l, l), letters)] == 0){
+            match(substr(d2[i,1], l, l), letters)] == 0){
           d2[i,2] <- 1
           stop = TRUE
           break
@@ -28,4 +29,7 @@ for(i in 1:nrow(d2)){
   }
 }
 
-tail(d2, 50)
+table(as.numeric(d2[,2]))
+
+sum(d2[,2])
+
