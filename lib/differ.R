cd50dbe87703ca05f14b1e726514072fc21e34cb
yadir <yl3435@columbia.edu>
2018-11-24 17:53:20 -0500

    Update differ.R

--- lib/differ.R
+++ lib/differ.R
@@ -1,9 +1,30 @@
 load(file = "../output/dict.RData")
 
-# w <- "cat"
+# This file includes a function that generates a list of words that differ by zero, one, or two characters from a given word
 
-# @cand this is fnowpd
-# @end
+# Finding words that only differ by one letter
+
+# @w this is the input word
+# @l this is the letter at position i 
+# @v this is a vector with all letters except l 
+# @beg this is a string of all the characters in a word before i
+# @end this is a string of all the characters in a word after i 
+@ #candidates this is the list of all possible strings that differ by one character, i, from w 
+@ #cand this is is the list of all possible strings that differ by one character,i, from w that match with a word in a dictionary 
+
+# @Finding words that differ by two letters
+
+# @w this is the input word
+# @l_i this is the letter at position i 
+# @l_j this is the letter at position j
+# @v_i this is a vector with all letters except l_i
+# @v_j this is a vector with all letters except l_j
+# @beg this is a string of all the characters in a word before i
+# @mid this is a string of all the chracters in a word between i and j
+# @end this is a string of all the characters in a word after j
+@ #candidates this is the list of all possible strings that differ by two characters, i, and j from w
+@ #cand this is is the list of all possible strings that differ by characters i,j, from w that match with a word in a dictionary 
+
 
 differCandidates <- function(w){
   
@@ -33,5 +54,5 @@ differCandidates <- function(w){
       }
     }
   }
-  return(cand)
+  return (paste(cand, w)) 
 }
