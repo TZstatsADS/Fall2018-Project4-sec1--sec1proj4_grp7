3cafe12648afdf9414a3102f7e926d39978668e1
XiaojieWei <weixiaojie7782@163.com>
2018-11-27 11:53:52 -0500

    dictionary

--- lib/Dictionary.R
+++ lib/Dictionary.R
@@ -4,31 +4,33 @@ library(dplyr)
 library(tidytext)
 library(broom)
 
-files <- list.files(path="../data/ground_truth/", pattern="*.txt", full.names=TRUE, recursive=FALSE)
-groundTruth <- ""
+dictionary <- function(truthTrain){
+  
+  groundTruth <- ""
+  for(x in truthTrain) groundTruth <- paste(groundTruth, readChar(x, file.info(x)$size))
+  
+  groundTruth <- strsplit(groundTruth,"\n")[[1]]
+  groundTruth <- groundTruth[groundTruth!=""]
+  bag <- str_split(groundTruth," ")
+  bag <- unlist(bag)
+  toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+  
+  corpus <- VCorpus(VectorSource(bag))%>%
+    tm_map(content_transformer(tolower))%>%
+    tm_map(toSpace, "\\W")%>%                             
+    tm_map(removePunctuation)%>%
+    tm_map(removeNumbers)%>%
+    tm_map(removeWords, character(0))%>%
+    tm_map(stripWhitespace)
+  
+  dict <- tidytext::tidy(corpus) %>%
+    select(text) %>%
+    unnest_tokens(dictionary, text)
+  
+  dict <- as.matrix(dict)
+  dict <- dict[nchar(dict) > 1] # no single character words
+  dict <- unique(dict)
+  
+  save(dict, file = "../output/dict.RData")
+}
 
-for(x in files) groundTruth <- paste(groundTruth, readChar(x, file.info(x)$size))
-
-groundTruth <- strsplit(groundTruth,"\n")[[1]]
-groundTruth <- groundTruth[groundTruth!=""]
-bag <- str_split(groundTruth," ")
-bag <- unlist(bag)
-toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
-
-corpus <- VCorpus(VectorSource(bag))%>%
-  tm_map(content_transformer(tolower))%>%
-  tm_map(toSpace, "\\W")%>%                             
-  tm_map(removePunctuation)%>%
-  tm_map(removeNumbers)%>%
-  tm_map(removeWords, character(0))%>%
-  tm_map(stripWhitespace)
-
-dict <- tidytext::tidy(corpus) %>%
-  select(text) %>%
-  unnest_tokens(dictionary, text)
-
-dict <- as.matrix(dict)
-dict <- dict[nchar(dict) > 1] # no single character words
-dict <- unique(dict)
-
-save(dict, file = "../output/dict.RData")
--- lib/OCRText.R
+++ lib/OCRText.R
@@ -8,30 +8,36 @@ library(dplyr)
 library(tidytext)
 library(broom)
 
-files <- list.files(path="../data/tesseract/", pattern="*.txt", full.names=TRUE, recursive=FALSE)
-tesseract <- ""
 
-for(x in files) tesseract <- paste(tesseract, readChar(x, file.info(x)$size))
+OCRText <- function(ocrTrain){
+  
+  tesseract <- ""
+  
+  for(x in ocrTrain) tesseract <- paste(tesseract, readChar(x, file.info(x)$size))
+  
+  tesseract <- strsplit(tesseract,"\n")[[1]]
+  tesseract <- tesseract[tesseract!=""]
+  bag <- str_split(tesseract," ")
+  bag <- unlist(bag)
+  toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+  
+  corpus <- VCorpus(VectorSource(bag))%>%
+    tm_map(content_transformer(tolower))%>%
+    tm_map(toSpace, "\\W")%>%
+    tm_map(removePunctuation)%>%
+    tm_map(removeWords, character(0))%>%
+    tm_map(stripWhitespace)
+  
+  dict2 <- tidytext::tidy(corpus) %>%
+    select(text) %>%
+    unnest_tokens(dictionary, text)
+  
+  dict2 <- as.matrix(dict2)
+  dict2 <- cbind(dict2, rep(0, nrow(dict2)))
+  colnames(dict2) <- c("word", "error")
+  
+  save(dict2, file = "../output/dict2.RData")
+  
+}
 
-tesseract <- strsplit(tesseract,"\n")[[1]]
-tesseract <- tesseract[tesseract!=""]
-bag <- str_split(tesseract," ")
-bag <- unlist(bag)
-toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
 
-corpus <- VCorpus(VectorSource(bag))%>%
-  tm_map(content_transformer(tolower))%>%
-  tm_map(toSpace, "\\W")%>%
-  tm_map(removePunctuation)%>%
-  tm_map(removeWords, character(0))%>%
-  tm_map(stripWhitespace)
-
-dict2 <- tidytext::tidy(corpus) %>%
-  select(text) %>%
-  unnest_tokens(dictionary, text)
-
-dict2 <- as.matrix(dict2)
-dict2 <- cbind(dict2, rep(0, nrow(dict2)))
-colnames(dict2) <- c("word", "error")
-
-save(dict2, file = "../output/dict2.RData")
