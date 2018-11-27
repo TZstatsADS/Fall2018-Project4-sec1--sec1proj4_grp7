525b6f9848176d10d9b1512a0ba17d5428e8c617
Gabriel Bénédict <gbndict@gmail.com>
2018-11-27 13:09:19 -0500

    push

--- lib/LDA.R
+++ lib/LDA.R
@@ -1,36 +1,48 @@
-library(tm)
-library(topicmodels)
-library(tidytext)
-library(tidyverse)
+
+topicModelling <- function(files){
+  
+  textInFiles <- lapply(files, function (filename) read.table(filename, sep="\t", stringsAsFactors = F))
+  textInFiles <- lapply(textInFiles, function(x) x$V1)
+  
+  a <- Corpus(VectorSource(textInFiles), readerControl = list(language="lat")) #specifies the exact folder where my text file(s) is for analysis with tm.
+  a <- tm_map(a, tolower)
+  a <- tm_map(a , stripWhitespace)
+  a <- tm_map(a, removeWords, stopwords("en"))
+  # warnings are not relevant: https://stackoverflow.com/questions/51081415/transformation-drops-documents-error-in-r
+  
+  adtm <- DocumentTermMatrix(a)
+  l <- LDA(adtm, k = 30, control = list(seed = 1234))
+  
+  save(l, file = "../output/LDA.RData")
+  return(l)
+}
 
 
-a  <- Corpus(VectorSource(truthTrain), readerControl = list(language="lat")) #specifies the exact folder where my text file(s) is for analysis with tm.
-a <- tm_map(a, content_transformer(tolower))
-a <- tm_map(a , stripWhitespace)
-a <- tm_map(a, removeWords, sw)
 
-adtm <- DocumentTermMatrix(a)
-l <- LDA(adtm, k = 30, control = list(seed = 1234))
 
-ap_topics <- tidy(l, matrix = "beta")
+# plotting
 
-ap_top_terms <- ap_topics %>%
-  group_by(topic) %>%
-  top_n(20, beta) %>%
-  ungroup() %>%
-  arrange(topic, -beta)
+# ap_top_terms <- ap_topics %>%
+#   group_by(topic) %>%
+#   top_n(20, beta) %>%
+#   ungroup() %>%
+#   arrange(topic, -beta)
 
-ap_top_terms %>%
-  mutate(term = reorder(term, beta)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  coord_flip()
+# ap_top_terms %>%
+#   mutate(term = reorder(term, beta)) %>%
+#   ggplot(aes(term, beta, fill = factor(topic))) +
+#   geom_col(show.legend = FALSE) +
+#   facet_wrap(~ topic, scales = "free") +
+#   coord_flip()
+# 
+# beta_spread <- ap_topics %>%
+#   mutate(topic = paste0("topic", topic)) %>%
+#   spread(topic, beta) %>%
+#   filter(topic1 > .001 | topic2 > .001) %>%
+#   mutate(log_ratio = log2(topic2 / topic1))
 
-beta_spread <- ap_topics %>%
-  mutate(topic = paste0("topic", topic)) %>%
-  spread(topic, beta) %>%
-  filter(topic1 > .001 | topic2 > .001) %>%
-  mutate(log_ratio = log2(topic2 / topic1))
+# beta_spread
 
-beta_spread
+
+
+
