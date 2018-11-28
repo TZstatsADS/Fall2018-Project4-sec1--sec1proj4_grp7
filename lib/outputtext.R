c72af57b9d3cbf86221db0bdc5ef0e5ddfb4aac5
Gabriel Bénédict <gbndict@gmail.com>
2018-11-28 13:32:07 -0500

    perf

--- lib/outputtext.R
+++ lib/outputtext.R
@@ -11,10 +11,10 @@ outputtext <- function(ocrcorrect){
     data <- c()
     
     for (line in 1:nline){
-      datarow <- paste(paste(ocrcorrect[docnumber == doc & linenumber == line,1], collapse = ""))
+      datarow <- paste(paste(ocrcorrect[docnumber == doc & linenumber == line,1], collapse = " "))
       data <- c(data, datarow)
     }
-  write.table(data, file = paste("../output/correct/ocrcorrect", doc,".txt"),
+  write.table(data, file = paste0("../output/correct/ocrcorrect", doc,".txt"),
               sep = "", row.names = FALSE, col.names = FALSE)
-}
+  }
 }
