57186a2e6843967ff0fff11718f835e1d31d72fa
Gabriel Bénédict <gbndict@gmail.com>
2018-11-27 22:32:39 -0500

    ocrtext

--- lib/OCRText.R
+++ lib/OCRText.R
@@ -11,30 +11,58 @@ library(broom)
 
 OCRText <- function(ocrTrain){
   
-  tesseract <- ""
-  for(x in ocrTrain) tesseract <- paste(tesseract, readChar(x, file.info(x)$size))
+  # check how many words tesseract has
+  tessWords <- ""
+  for(x in ocrTrain) tessWords <- paste(tessWords, readChar(x, file.info(x)$size))
+  tessWords <- strsplit(tessWords,"\n")[[1]]
+  bag <- strsplit(tessWords," ")
+  n <- length(unlist(bag))
   
-  tesseract <- strsplit(tesseract,"\n")[[1]]
-  tesseract <- tesseract[tesseract!=""]
-  bag <- str_split(tesseract," ")
-  bag <- unlist(bag)
+  
+  tesseract <- c()
+  i <- 1
+  for(x in ocrTrain){
+    tesseract[i] <- readChar(x, file.info(x)$size)
+    i <- i + 1
+  }
+  
+  tesseract <- strsplit(tesseract,"\n")
+  # tesseract <- tesseract[tesseract!=""]
+  
+  for(i in 1 : length(tesseract)){
+    tesseract[[i]] <- strsplit(tesseract[[i]]," ")
+  }
+  
+  bag <- matrix(NA, ncol = 5, nrow = n, dimnames = list(rep("", n), c("token", "word", "line", "doc", "error")))
+  i <- 1
+  for(d in 1:length(tesseract)){ # for each doc
+    for(l in 1:length(tesseract[[d]])){ # for each line
+      for(w in 1:length(tesseract[[d]][[l]])){ # for each word
+        bag[i,] <- c(tesseract[[d]][[l]][w], w, l, d, 0)
+        i <- i + 1
+      }
+    }
+  }
+  
+  bag <- bag[complete.cases(bag),]
+  
   toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
   
-  corpus <- VCorpus(VectorSource(bag))%>%
+  corpus <- VCorpus(VectorSource(bag[,1]))%>%
     tm_map(content_transformer(tolower))%>%
-    tm_map(toSpace, "\\W")%>%
+    # tm_map(toSpace, "\\W")%>%
     tm_map(removePunctuation)%>%
     tm_map(removeWords, character(0))%>%
     tm_map(stripWhitespace)
   
-  dict2 <- tidytext::tidy(corpus) %>%
+  ocrText <- tidytext::tidy(corpus) %>%
     select(text) %>%
     unnest_tokens(dictionary, text)
   
-  dict2 <- as.matrix(dict2)
-  OCRText <- cbind(dict2, rep(0, nrow(dict2)))
-  colnames(OCRText) <- c("word", "error")
-  
+  library(textreg)
+  b <- convert.tm.to.character(corpus)
+  bag[,1] <- b
+  OCRText <- bag
   save(OCRText, file = "../output/OCRText.RData")
 }
 
